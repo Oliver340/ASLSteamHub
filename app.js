68d5b6931bf2146204227c627455dca9899d2d94
Mazianos <mazin.marwan@gmail.com>
2022-03-23 17:08:12 -0700

    Added route logic

--- app.js
+++ app.js
@@ -3,9 +3,22 @@
 // Import the functions you need from the SDKs you need
 const express = require('express');
 const path = require('path');
+const bcrypt = require('bcrypt');
+const mysql = requrie('mysql');
+const jwt = require("jsonwebtoken");
+
 const server = express();
 const port = 4757;
 const updir = '..';
+const saltRounds = 12;
+const secretKey = "X0x1fAHRJojFrRGw16XJ";
+const connection = mysql.createConnection({
+    host: "localhost:3306",
+    user: "antoi_aslsteam",
+    password: "1D1^o2or",
+    database: "antoinette_jackson_bcit_ca_aslsteamhub "
+});
+connection.connect();
 
 server.use('/html', express.static(path.join(__dirname, "html")));
 server.use('/css', express.static(path.join(__dirname, "css")));
@@ -28,34 +41,116 @@ server.get('/', (req, res) => {
 //*******MAIN API PORTION*******//
 
 server.post('/api/signup', (req, res) => {
-  try {
-    const email = req.body.email;
-    const name = req.body.name;
-    
-    const password = bcrypt.hash(req.body.password, saltRounds, (err, salt) => {
-        connection.query(`INSERT INTO Users ('Email', 'Password', 'Full Name') VALUES ('${email}', '${password}', '${name}');`, (err, result) => {
-            if (err) throw err;
-            let token = generateLoginToken();
-            connection.query(`UPDATE Users SET 'Login Token' = ${token} WHERE 'Email' = '${email}'`, (err, result) => {
+    try {
+        const email = req.body.email;
+        const name = req.body.name;
+
+        const password = bcrypt.hash(req.body.password, saltRounds, (err, salt) => {
+            connection.query(`INSERT INTO User ('Email', 'Password', 'Full Name') VALUES ('${email}', '${password}', '${name}');`, (err, result) => {
                 if (err) throw err;
-                connection.query(`INSERT INTO Players ('Player ID', 'Full Name', 'DOB', 'Sex', 'Skill Level', 'Allergies/Medication', 'Emergency Contact Number', 'Email')
-                    VALUES (NULL, ${name}, ${dateOfBirth}, ${sex}, ${skill}, ${allergies}, ${emergency}, ${email});`, (err, result) => {
-                    if (err) throw err;
-                    res.send({
-                        code: 200,
-                        message: token
-                    });
-                });
+                connection.query(`SELECT Permissions FROM User WHERE Password = '${password}'`, (err, resp) =>{
+                    console.long
+                    res.json(jwt.sign({
+                        password: password,
+                        admin: resp
+                    }, secretKey, {
+                        expiresIn: "12h",
+                    }));
+                })
             });
         });
-    });
-} catch {
-    res.send({
-        code: 400,
-        message: "An error occurred. :("
-    });
-}
+    } catch {
+        res.json({
+            code: 400,
+            message: "An error occurred. :("
+        });
+    }
 });
+
+server.post('/api/signIn', (req, res) => {
+    try {
+        connection.query(`SELECT Password, Permissions, UserID FROM Users WHERE Email '= ${req.body.email}'`, (e, r) => {
+            if (e) throw e;
+            if (bcrypt.compareSync(req.body.password, r)) {
+                res.json(jwt.sign(r, secretKey, {
+                    expiresIn: "12h",
+                }));
+            }
+        });
+    } catch {
+        res.json({
+            code: 400,
+            message: "Something went wrong"
+        });
+    }
+});
+
+server.post('/api/lists', (req, res) => {
+    let permission = validate(req.body.token);
+    if (permission) {
+        //Look for words
+        connection.query(`SELECT Word.Word, Word.PlainDef, Word.TechDef, Word.VideoLink, List.ListID
+            FROM Word 
+            LEFT JOIN LinkedList ON Word.WordID = LinkedList.WordID
+            LEFT JOIN List ON List.ListID = LinkedList.WordID
+            WHERE List.UserID = ${permission.UserID}`, (err, result) => {
+                    if (err) throw err;
+                    res.json(result);
+                });
+    }
+});
+
+server.post('/api/addWord', (req, res) => {
+    let permission = validate(req.body.token);
+    if (permission) {
+        connection.query(`INSERT INTO LinkedList (ListID, WordID) VALUES (${permission.ListID}, ${permission.WordID})`, (req, res) => {
+            res.json({
+                code: 200,
+                message: "Word added successfully"
+            });
+        });
+    }
+})
+
+server.get('/api/library', (req, res) => {
+    let permission = validate(req.body.token);
+    if (permission) {
+        connection.query(`SELECT Word, PlainDef, TechDef, VideoLink FROM Word WHERE Status = APPROVED`, (err, result) => {
+            res.json(result);
+        });
+    }
+});
+
+server.post('/api/admin', (req, res) => {
+    let permission = validate(req.body.token);
+    if (permission) {
+        if (permission.Permissions == "ADMIN") {
+            connection.query(`SELECT WordID, Word, PlainDef, TechDef, VideoLink FROM Word WHERE Status = PENDING`, (err, result) => {
+                if (err) throw err;
+                res.json(result);
+            });
+        }
+    }
+})
+
+server.post('/api/settings', (req, res) => {
+    let permission = validate(req.body.token);
+    if (permission) {
+        //Update user data
+    }
+});
+
+let validate = (token) => {
+    try {
+        let response = jwt.verify(token, secretKey);
+        return {
+            permissions: response.Permissions,
+            id: response.UserID
+        };
+    } catch {
+        return "Validation failed";
+    }
+}
 
 server.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
