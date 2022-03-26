df0649ca97db7d61e81b3cf54a606ced635c2115
Oliver Harrison <oliverharrison255@gmail.com>
2022-03-26 16:06:53 -0700

    fixed a thing

--- public/js/signIn.js
+++ public/js/signIn.js
@@ -1,9 +1,5 @@
 const loginForm = document.querySelector("#login");
 
-let login = function(email, password) {
-    //Send login info and store token returned in session storage
-}
-
 loginForm.addEventListener("submit", (e) => {
     e.preventDefault();
     let user = login(loginForm.emailEntry.value, loginForm.passwordEntry.value);
