2f4c744d55652ee1d11a39d4d27aafedc2611d9a
Oliver Harrison <oliverharrison255@gmail.com>
2022-03-26 16:50:32 -0700

    fix admin

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ module.exports = (router) => {
     });
 
     router.get('/signup', (req, res) => {
-        res.sender('signup');
+        res.render('signup');
     });
     
     router.get('/editList', (req, res) => {
